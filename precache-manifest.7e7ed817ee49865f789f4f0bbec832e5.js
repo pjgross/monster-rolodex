a5c9f12a42bd7174649805a40e03d032a24053e5
Petegross <pete.gross@btinternet.com>
2019-07-17 10:24:15 +0100

    Updates

---
+++ precache-manifest.7e7ed817ee49865f789f4f0bbec832e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bfdb21b8be4aa0d8a79e8e56853e59",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3fed107a5760ed280872",
+    "url": "/monster-rolodex/static/css/main.3ea8d013.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3fed107a5760ed280872",
+    "url": "/monster-rolodex/static/js/main.a4899783.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
